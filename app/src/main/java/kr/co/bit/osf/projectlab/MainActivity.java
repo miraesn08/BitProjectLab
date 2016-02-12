a4f869309bc5151569edcb0be32acff9b4d98024
miraesn08 <miraesn08@gmail.com>
2016-02-12 13:13:51 +0900

    add ImageUtil Button

--- app/src/main/java/kr/co/bit/osf/projectlab/MainActivity.java
+++ app/src/main/java/kr/co/bit/osf/projectlab/MainActivity.java
@@ -11,23 +11,31 @@ public class MainActivity extends AppCompatActivity {
 
     final String TAG = "projectLabTag";
     Button btnCamera = null;
+    Button btnImageUtil = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCamera = (Button)findViewById(R.id.btnCamera);
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+        btnImageUtil = (Button) findViewById(R.id.btnImageUtil);
+
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "btnCamera clicked");
                 Intent cameraLabIntent = new Intent(getApplicationContext(), CameraActivity.class);
                 startActivity(cameraLabIntent);
+            }
+        });
 
-                // http://www.tutorialspoint.com/android/android_camera.htm
-                // Using existing android camera application in our application
-                //Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        btnImageUtil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "btnImageUtil clicked");
+                Intent intent = new Intent(getApplicationContext(), ImageUtilActivity.class);
+                startActivity(intent);
             }
         });
     }
