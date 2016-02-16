0ba3bcc7ec0b03c99cd32b4e6b4774c2887afa3f
miraesn08 <miraesn08@gmail.com>
2016-02-17 08:34:06 +0900

    add BoxDAO(interface), CardDAO(interface), StateDAO(interface)

---
+++ app/src/main/java/kr/co/bit/osf/projectlab/db/CardDAO.java
@@ -0,0 +1,11 @@
+package kr.co.bit.osf.projectlab.db;
+
+import java.util.List;
+
+public interface CardDAO {
+    boolean addCard(CardDTO card);
+    CardDTO getCard(int id);
+    boolean deleteCard(int id);
+    boolean updateCard(CardDTO newValue);
+    List<CardDTO> getCardByBoxId(int boxId);
+}
