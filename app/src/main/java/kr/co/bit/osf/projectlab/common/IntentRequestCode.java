8d97eaf4b24287422ea42998677c3212c022a99d
miraesn08 <miraesn08@gmail.com>
2016-02-18 10:20:56 +0900

    add IntentRequestCode

---
+++ app/src/main/java/kr/co/bit/osf/projectlab/common/IntentRequestCode.java
@@ -0,0 +1,9 @@
+package kr.co.bit.osf.projectlab.common;
+
+public class IntentRequestCode {
+    public static final int CAPTURE_IMAGE = 501;
+    public static final int SELECT_PICTURE = 601;
+    public static final int CARD_ADD = 701;
+    public static final int CARD_DELETE = 702;
+    public static final int CARD_EDIT = 703;
+}
