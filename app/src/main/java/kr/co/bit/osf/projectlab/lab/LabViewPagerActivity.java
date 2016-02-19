b5bf7956df09ebb358b88943250ea0a5b1806672
miraesn08 <miraesn08@gmail.com>
2016-02-19 09:07:40 +0900

    add LabViewPagerActivity

---
+++ app/src/main/java/kr/co/bit/osf/projectlab/lab/LabViewPagerActivity.java
@@ -0,0 +1,15 @@
+package kr.co.bit.osf.projectlab.lab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import kr.co.bit.osf.projectlab.R;
+
+public class LabViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_view_pager);
+    }
+}
