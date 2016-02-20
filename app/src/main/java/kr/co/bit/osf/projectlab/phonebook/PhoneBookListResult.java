1d4a8b5382b5f9bec233f811bba578f317d19d20
miraesn08 <miraesn08@gmail.com>
2016-02-20 10:16:21 +0900

    add LabRetrofitActivity - phonebook - vo, result, list result

---
+++ app/src/main/java/kr/co/bit/osf/projectlab/phonebook/PhoneBookListResult.java
@@ -0,0 +1,34 @@
+package kr.co.bit.osf.projectlab.phonebook;
+
+import java.util.List;
+
+public class PhoneBookListResult {
+	private int errorCode;
+	private String errorMessage;
+	private List<PhoneBookVO> phoneBookList;
+	
+	public int getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	public List<PhoneBookVO> getPhoneBookList() {
+		return phoneBookList;
+	}
+	public void setPhoneBookList(List<PhoneBookVO> phoneBookList) {
+		this.phoneBookList = phoneBookList;
+	}
+	
+	@Override
+	public String toString() {
+		return "PhoneBookListResult [errorCode=" + errorCode + ", errorMessage=" + errorMessage + ", phoneBookList="
+				+ phoneBookList + "]";
+	}
+}
