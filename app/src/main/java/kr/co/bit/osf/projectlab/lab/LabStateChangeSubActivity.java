f8fc4cf953643ad1916661b23e8aa469c7dea040
miraesn08 <miraesn08@gmail.com>
2016-02-22 10:04:35 +0900

    add LabStateChangeSubActivity

---
+++ app/src/main/java/kr/co/bit/osf/projectlab/lab/LabStateChangeSubActivity.java
@@ -0,0 +1,66 @@
+package kr.co.bit.osf.projectlab.lab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import kr.co.bit.osf.projectlab.R;
+import kr.co.bit.osf.projectlab.debug.Dlog;
+
+public class LabStateChangeSubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_state_change_main);
+        Dlog.i("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Dlog.i("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Dlog.i("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Dlog.i("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Dlog.i("onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Dlog.i("onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Dlog.i("onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Dlog.i("onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Dlog.i("onRestoreInstanceState");
+    }
+
+}
